642d995509885adc693040886dd2bd8d4cb4a20d
Svyatoslav Chukhlebov <slavik@wlan-t-244-94-dhcp.CSUChico.EDU>
2020-02-12 11:21:30 -0800

    Fixed issues "Project is completely undocumented" and "Negative Inventory Bug"

--- Inventory.h
+++ Inventory.h
@@ -5,6 +5,18 @@
 using std::string;
 using std::ostream;
 
+/*
+Summary of class Inventory:
+Keeps track of an inventory item by name, including price and stock.
+
+String m_name: Name of the inventory item
+Float m_price: Price of the inventory item
+Int m_in_stock: Quantity of the inventory item
+
+Void sell() subtracts one from the stock quantity
+The item price can be be printed from outside of this class using <<InventoryInstance
+*/
+
 class Inventory
 {
   private:
